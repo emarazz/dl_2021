6817bd7f6a809abb6568dec796c159ca9b60f078
Gloria <gloria.dalsanto@outlook.it>
2021-04-28 18:34:44 +0200

    array with loss values

--- Proj1/impl_02/train.py
+++ Proj1/impl_02/train.py
@@ -7,20 +7,22 @@ ToDo: in optim.SGD add momentum and nesterov options
 """
 def train_BaseNet(model, train_loader ,criterion, eta, epochs, optimizer):
     acc_loss = 0
+    loss = []
     if optimizer == "SGD":    
         optimizer = torch.optim.SGD(model.parameters(), lr = eta) 
         
     for e in range(epochs):
         for input_data, target, _ in iter(train_loader):
             output = model(input_data)
-            loss = criterion(output, target)
+            temp = criterion(output, target)
+            loss.append(temp)
             acc_loss = acc_loss + loss.item()
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
     
-    return acc_loss
+    return acc_loss, loss 
 
 def compute_acc_BaseNet(model, data_loader):
     nb_errors = 0
