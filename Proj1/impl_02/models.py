34c90a68145ce3ea52bed3223ecbb10934f7c1e8
Daniel Filipe Nunes Silva <danielementary@gmail.com>
2021-05-07 13:35:13 +0200

    make optional max_pool in basenet

--- Proj1/impl_02/models.py
+++ Proj1/impl_02/models.py
@@ -8,9 +8,9 @@ from torch.nn import functional as F
 from helpers import *
 from data import *
 
-# most basic architecture inspired from practical 4 adapted to suit this use case with optional dropout
+# most basic architecture inspired from practical 4 adapted to suit this use case with optional maxpool and dropout
 class BaseNet(nn.Module):
-    def __init__(self, nb_hidden, dropout_prob=0):
+    def __init__(self, nb_hidden, max_pool=False, dropout_prob=0):
         super().__init__()
 
         self.conv1 = nn.Conv2d( 2, 32, kernel_size=5)
@@ -22,8 +22,10 @@ class BaseNet(nn.Module):
         self.dropout = nn.Dropout(dropout_prob)
     
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), kernel_size=3, stride=3))
-        x = F.relu(F.max_pool2d(self.conv2(x), kernel_size=2, stride=2))
+        if max_pool: x = F.max_pool2d(self.conv1(x), kernel_size=3, stride=3)
+        x = F.relu(x)
+        if max_pool: x = F.max_pool2d(self.conv1(x), kernel_size=2, stride=2)
+        x = F.relu(x)
         x = F.relu(self.fc1(x.view(-1, 256)))
         x = self.dropout(x)
         x = self.fc2(x)
